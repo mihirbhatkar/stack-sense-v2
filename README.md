25b9655f9ccc65f89e6f63b19c78699e551c8379
mihirbhatkar <mihirbhatkar87@gmail.com>
2024-10-20 17:36:58 +0530

    initial files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # stack-sense-v2
+
+An expense tracker app made for me specifically. Basically a passion project ig idk
---
+++ android/app/src/main/kotlin/com/example/stack_sense_v2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.stack_sense_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
